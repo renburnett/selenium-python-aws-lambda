cf3f1babc8227a988191277fd0a324a629d34815
Renard Burnett <renburnett@Renards-MacBook-Pro.local>
2018-12-21 18:06:57 -0800

    added module for speech interaction capabilities

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,5 @@
+
+from kexp_crawler import recent_songs
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.utils import is_request_type, is_intent_name
 from ask_sdk_core.handler_input import HandlerInput
@@ -11,9 +13,26 @@ class LaunchRequestHandler(AbstractRequestHandler):
 
      def handle(self, handler_input):
          # type: (HandlerInput) -> Response
-         speech_text = "Welcome to the Alexa Skills Kit, you can say hello!"
+         speech_text = "Welcome! I can tell you the song that's currently playing on Kay-Ee-Ex-Pea or I can tell you the last few songs played!"
 
          handler_input.response_builder.speak(speech_text).set_card(
-            SimpleCard("Hello World", speech_text)).set_should_end_session(
+            SimpleCard("Would you like to hear the recent songs on Kay-Ee-Ex-Pea?", speech_text)).set_should_end_session(
             False)
          return handler_input.response_builder.response
+
+class NowPlayingIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("NowPlayingIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        songs = recent_songs(event, context)
+        index = len(songs)
+
+        speech_text = "Hello World"
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Hello World", speech_text)).set_should_end_session(
+            True)
+        return handler_input.response_builder.response
